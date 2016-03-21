fe466e3ae36ef76859c0e96d0471fd62eb532a86
priscillamc <priscillamc@users.noreply.github.com>
2016-03-21 13:19:07 -0400

    Initial release, adds Google Analytics, copies CSS files

--- Readme.md
+++ Readme.md
@@ -4,7 +4,7 @@ UF CLAS UFL Brand
 Description
 -----------
 
-WordPress child theme, based on the 2016 University of Florida Brand design.
+WordPress child theme, based on the 2016 University of Florida Brand design. Maintained by CLAS IT.
 
 - Author: Al Kirby
 - Plugin URI: http://clas.ufl.edu/
@@ -23,6 +23,7 @@ Installation
 
 Changelog
 ---------
+- 1.0.0 - Initial release
 - 0.0.0 - Initial commit
 
 
@@ -30,7 +31,6 @@ To-Do List
 ----------
 
 
-Note
+Notes
 ==============================
 
-This theme is still under development. 
--- functions.php
+++ functions.php
@@ -9,11 +9,26 @@
 
 /**
  * Enqueue scripts and styles.
+ * 
+ * @since 0.0.1
  */
 function ufclas_brand_styles_scripts() {
 	
 	// Custom styles
-	wp_enqueue_style('brand', get_stylesheet_directory_uri() . '/style.css', false, '0.0.0', 'all');
+	wp_enqueue_style('brand', get_stylesheet_directory_uri() . '/style.css', false, '1.0.0', 'all');
 	
 }
 add_action( 'wp_enqueue_scripts', 'ufclas_brand_styles_scripts' );
+
+/**
+ * Adds Google Analytics tracking code for non-logged in users
+ *
+ * @since 1.0.0
+ */
+function ufclas_brand_analytics(){
+
+	if( !is_user_logged_in() ){
+		get_template_part('inc/google-analytics');
+	}
+}
+add_action('wp_head', 'ufclas_brand_analytics');
